5639d5ba1c1e0837163533af51f2c38ce8317d6c
Wouter <wouter_hilversum@hotmail.com>
2025-03-21 21:11:31 +0100

    Split up controllers

--- tst/Authorization/IdentityProviderMockService/Controllers/JwksController.cs
+++ tst/Authorization/IdentityProviderMockService/Controllers/JwksController.cs
@@ -11,13 +11,13 @@ namespace IdentityProviderMockService.Controllers
     [Produces(MediaTypeNames.Application.Json)]
     [Route(".well-known")]
     [ApiController]
-    public class WellKnownController : ControllerBase
+    public class JwksController : ControllerBase
     {
-        private readonly ILogger<WellKnownController> _logger;
+        private readonly ILogger<OpenIdConfigurationController> _logger;
         private readonly RSAParameters _rsaParameters;
         private readonly AuthenticationOptions _authenticationOptions;
 
-        public WellKnownController(ILogger<WellKnownController> logger,
+        public JwksController(ILogger<OpenIdConfigurationController> logger,
             IOptions<AuthenticationOptions> authenticationOptions,
             RSA rsa)
         {
@@ -25,22 +25,6 @@ namespace IdentityProviderMockService.Controllers
             _authenticationOptions = authenticationOptions.Value;
 
             _rsaParameters = rsa.ExportParameters(includePrivateParameters: true);
-        }
-
-        [HttpGet("openid-configuration")]
-        [ProducesResponseType(typeof(UserResponse), StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public IActionResult GetOpenIdConfiguration()
-        {
-            _logger.LogDebug("OpenIdConfiguration is returned");
-
-            var discoveryDocument = new DiscoveryDocumentResponse
-            {
-                Issuer = _authenticationOptions.Issuer,
-                JwksUri = $"{_authenticationOptions.Issuer}/.well-known/jwks.json"
-            };
-
-            return Ok(discoveryDocument);
         }
 
         [HttpGet("jwks.json")]
