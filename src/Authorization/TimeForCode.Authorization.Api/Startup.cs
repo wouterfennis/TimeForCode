8f0b5a17dffd11fe6426c292d3ff50f034cfd1a7
Wouter <wouter_hilversum@hotmail.com>
2025-04-01 20:57:31 +0200

    formatting

--- src/Authorization/TimeForCode.Authorization.Api/Startup.cs
+++ src/Authorization/TimeForCode.Authorization.Api/Startup.cs
@@ -88,7 +88,7 @@ namespace TimeForCode.Authorization.Api
             {
                 app.UseDeveloperExceptionPage();
             }
-            
+
             app.UseHttpsRedirection();
 
             app.UseSwagger();
