9f558ee29dc0321db68637dcd84b2113982b8e10
wouter_hilversum@hotmail.com <wouter_hilversum@hotmail.com>
2025-04-15 20:51:59 +0200

    less tricky evaluation

--- src/Authorization/TimeForCode.Authorization.Commands/Result.cs
+++ src/Authorization/TimeForCode.Authorization.Commands/Result.cs
@@ -4,18 +4,28 @@ namespace TimeForCode.Authorization.Commands
     {
         private T? _value { get; init; }
         private string? _errorMessage { get; init; }
-        public T Value => _value ?? throw new InvalidOperationException("The result does not contain any value.");
+        public T Value => _value!;
         public bool IsSuccess { get; init; }
         public bool IsFailure => !IsSuccess;
-        public string ErrorMessage => _errorMessage ?? throw new InvalidOperationException("The result does not contain any error message.");
+        public string ErrorMessage => _errorMessage!;
 
         public static Result<T> Success(T data)
         {
+            if (data is null)
+            {
+                throw new InvalidOperationException("The result does not contain any value.");
+            }
+
             return new Result<T> { _value = data, IsSuccess = true };
         }
 
         public static Result<T> Failure(string? errorMessage)
         {
+            if (string.IsNullOrEmpty(errorMessage))
+            {
+                throw new InvalidOperationException("The result does not contain any error message.");
+            }
+
             return new Result<T> { IsSuccess = false, _errorMessage = errorMessage };
         }
     }
