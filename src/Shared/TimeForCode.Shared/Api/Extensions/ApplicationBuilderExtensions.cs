c2fea406f7795a0a88b9be41a8e66af1dc003405
Wouter <wouter_hilversum@hotmail.com>
2026-07-11 23:41:10 +0200

    b

--- src/Shared/TimeForCode.Shared/Api/Extensions/ApplicationBuilderExtensions.cs
+++ src/Shared/TimeForCode.Shared/Api/Extensions/ApplicationBuilderExtensions.cs
@@ -1,7 +1,7 @@
 using FluentValidation;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Hosting;
 using Scalar.AspNetCore;
